b7bd7d8c187a50e3188589ad69571f1dce55c34e
GhostDragon14 <106036383+GhostDragon14@users.noreply.github.com>
2022-05-22 16:18:33 -0500

    Create PolygonInAPolygon.py

---
+++ PolygonInAPolygon.py
@@ -0,0 +1,60 @@
+import turtle
+
+colors = ["blue","yellow","orange","grey","red","dark pink","purple","green","brown"]
+
+t=turtle.Pen()
+turtle.bgcolor('black')
+t.speed(10000000)
+for x in range(450):
+    t.forward(x)
+    t.left(45)
+    t.pencolor(colors[0])
+    t.width(5)
+t.home()
+for x in range(400):
+    t.forward(x)
+    t.left(45)
+    t.pencolor(colors[1])
+    t.width(5)
+t.home()
+for x in range(350):
+    t.forward(x)
+    t.left(45)
+    t.pencolor(colors[2])
+    t.width(5)
+t.home()
+for x in range(300):
+    t.forward(x)
+    t.left(45)
+    t.pencolor(colors[3])
+    t.width(5)
+t.home()
+for x in range(250):
+    t.forward(x)
+    t.left(45)
+    t.pencolor(colors[4])
+    t.width(5)
+t.home()
+for x in range(200):
+    t.forward(x)
+    t.left(45)
+    t.pencolor(colors[5])
+    t.width(5)
+t.home()
+for x in range(150):
+    t.forward(x)
+    t.left(45)
+    t.pencolor(colors[6])
+    t.width(5)
+t.home()
+for x in range(100):
+    t.forward(x)
+    t.left(45)
+    t.pencolor(colors[7])
+    t.width(5)
+t.home()
+for x in range(50):
+    t.forward(x)
+    t.left(45)
+    t.pencolor(colors[8])
+    t.width(5)
